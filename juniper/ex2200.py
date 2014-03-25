96a3aa0348e3266785a8872fe36010521e0c7032
revehs <revehs@gmail.com>
2014-03-25 18:56:19 +0900

    0325-2

--- juniper/ex2200.py
+++ juniper/ex2200.py
@@ -5,8 +5,11 @@ dev.open()
 
 
 cu = Config(dev)
-set_cmd = 'set system login message "0324"'
-print cu.load(set_cmd, format='set')
+#set_cmd = {"set system login message 'hi_0325'":
+#           "set interfaces vlan unit 3150 family inet address 20.0.0.1/24"}
+
+
+print cu.load(path="/home/hans/git/juniper/juniper/config.set", format='set')
 print cu.pdiff()
 
 #print cu.commit_check()
